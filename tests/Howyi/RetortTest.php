0b363f8c5c1445565c65ce09503819d30e75a186
howyi <qrbys@outlook.com>
2017-10-12 23:27:31 +0900

    change test order

--- tests/Howyi/RetortTest.php
+++ tests/Howyi/RetortTest.php
@@ -4,15 +4,15 @@ namespace Howyi;
 
 class RetortTest extends \PHPUnit\Framework\TestCase
 {
-    public function testHeat()
-    {
-        Retort::heat();
-        $this->assertTrue(true);
-    }
-
     public function testSeal()
     {
         Retort::seal();
+        $this->assertTrue(true);
+    }
+
+    public function testHeat()
+    {
+        Retort::heat();
         $this->assertTrue(true);
     }
 }
