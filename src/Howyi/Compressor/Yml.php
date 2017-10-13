399787430c60ea9e9ca6d7b70af42ac8573bd9ca
howyi <qrbys@outlook.com>
2017-10-13 13:59:19 +0900

    add yaml compressor

---
+++ src/Howyi/Compressor/Yml.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Howyi\Compressor;
+
+use Symfony\Component\Yaml\Yaml as SymfonyYaml;
+
+class Yml implements CompressorInterface
+{
+    public static function extract(string $path)
+    {
+        $currentPath = rtrim($path, '.yml');
+        $contents = SymfonyYaml::parse(file_get_contents($path));
+        foreach ($contents as $filePath => $content) {
+            $dirPath = dirname($currentPath . DIRECTORY_SEPARATOR . $filePath);
+            if (!file_exists($dirPath)) {
+                mkdir($dirPath, 0777, true);
+            }
+            file_put_contents($currentPath . DIRECTORY_SEPARATOR . $filePath, $content);
+        }
+    }
+
+    public static function archive(string $path)
+    {
+        $ymlPath = "$path.yml";
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($path),
+            \RecursiveIteratorIterator::LEAVES_ONLY
+        );
+        $contents = [];
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($path) + 1);
+                $contents[$relativePath] = file_get_contents($filePath);
+            }
+        }
+
+        file_put_contents($ymlPath, SymfonyYaml::dump($contents, 4, 2));
+
+    }
+}
